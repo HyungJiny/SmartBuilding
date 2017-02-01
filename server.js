b442ae55d2935999cef3d03e1462295400e8f900
HyungJiny <bbqnn200@gmail.com>
2017-02-01 22:23:48 +0000

    add main page templets

--- server.js
+++ server.js
@@ -3,3 +3,11 @@ var app = express()
 app.locals.pretty = true;
 app.set('view engine', 'jade');
 app.set('views', '/views');
+
+app.use(express.static('public'))
+app.get('/', function(req, res){
+	res.send("Hello!");
+});
+app.listen(3000, function(){
+	console.log('Connected 3000 port!');
+});
