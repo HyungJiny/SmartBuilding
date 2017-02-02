8b639de1bd4399472e48d07872a41ac70d4ead1a
HyungJiny <bbqnn200@gmail.com>
2017-02-02 15:57:42 -0500

    add sign button

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 var express = require('express');
+var bodyParser = require('body-parser');
 var app = express()
 app.locals.pretty = true;
 app.set('view engine', 'jade');
 app.set('views', '/views');
 
-app.use(express.static('public'))
+app.use(express.static('public'));
+app.use(bodyParser.urlencoded({ extended: false }));
 app.get('/', function(req, res){
 	res.send("Hello!");
 });
