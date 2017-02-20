710110392bbe07f954c510e54357e6aa45e833e5
HyungJiny <bbqnn200@gmail.com>
2017-02-20 15:01:54 -0500

    changing db server

--- config.js
+++ config.js
@@ -1,4 +1,5 @@
 module.exports = {
   'secret' : 'SeCrEtKeYfOrHaShInG',
-  'mongodbUri': 'mongodb://hyungjin:gudwls@ds153179.mlab.com:53179/smartbuilding'
+  // 'mongodbUri': 'mongodb://hyungjin:gudwls@ds153179.mlab.com:53179/smartbuilding'
+  'mongodbUri': 'mongodb://54.145.186.154:27017'
 }
